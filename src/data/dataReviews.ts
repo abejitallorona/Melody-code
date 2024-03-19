beede02af27d2970c64ead068439120bad87a111
Lalauraph <laurapalacio900@gmail.com>
2024-03-18 22:39:57 -0500

    "chore: data docs"

---
+++ src/data/dataReviews.ts
@@ -0,0 +1,12 @@
+export const Reviews = [
+	{
+		nameAndArtist: 'Setlle-Disclosure',
+		TypeAndYear: 'Album-2013',
+		UserPicture: '',
+		UserName: 'Laura Palacio',
+		Review:
+			"Disclosure's  Settle is an electrifying masterpiece that seamlessly blends house, garage, and pop elements, creating a sonic experience that feels both nostalgic and groundbreaking. From the infectious beats of  Latch  to the soulful vocals on White Noise, each track is a journey in itself The duo's production prowess shines through, with tight rhythms, catchy hooks, and a keen sense of melody F For You and When a Fire Starts to Burn showcase their ability to craft club anthems that still resonate with depth The collaboration with talented vocalists adds another layer, elevating the album to a truly immersive experience...",
+		Likes: '127',
+		Comments: '236',
+	},
+];
