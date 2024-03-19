fb0fe6f7044d85be92a1448eea2a25ce7abd96b9
Lalauraph <laurapalacio900@gmail.com>
2024-03-19 16:21:28 -0500

    "fix: data"

--- src/components/banner/banner.ts
+++ src/components/banner/banner.ts
@@ -1,37 +1,51 @@
+export enum Attributes {
+	'Username' = 'Username',
+	'ProfilePicture' = 'ProfilePicture',
+}
+
 class Banner extends HTMLElement {
+	Username?: string;
+	ProfilePicture?: string;
+
 	constructor() {
 		super();
 		this.attachShadow({ mode: 'open' });
 	}
 
 	static get observedAttributes() {
-		return ['username', 'userpicture', 'serach-bar'];
+		const attrs: Record<Attribute, null> = {
+			Username: null,
+			ProfilePicture: null,
+		};
+		return Object.keys(attrs);
 	}
 
 	connectedCallback() {
 		this.render();
 	}
 
-	attributeChangedCallback(propName, oldvlue, newValue) {
-		this[propName] = newValue;
+	attributeChangedCallback(propName: string, oldValue: string | null, newValue: string | null) {
+		if (newValue !== null) {
+			this[propName] = newValue;
+		}
 	}
 
 	render() {
-		//QUEMANDO CONTENIDO!!
-		if (this.shadowRoot)
+		// Agregar el contenido HTML
+		if (this.shadowRoot) {
 			this.shadowRoot.innerHTML = `
-          <section class="banner-section">
-              <img src="https://i.pinimg.com/564x/0c/dc/42/0cdc42ae52de52c6ebc16f1f23b3dabd.jpg">
-              <h3>LAURIX</h3>
-              <img src="https://co.pinterest.com/pin/703265298091791024">
-              <form id="search-form">
-                    <input type="text" name="search" placeholder="Search...">
-                    <button type="submit">Search</button>
-                </form>
-          </section>
-      `;
+              <section class="banner-section">
+                  <img src="https://i.pinimg.com/564x/0c/dc/42/0cdc42ae52de52c6ebc16f1f23b3dabd.jpg">
+                  <h3>${this.Username}</h3>
+                  <img src="https://co.pinterest.com/pin/703265298091791024">
+                  <form id="search-form">
+                      <input type="text" name="search" placeholder="Search...">
+                      <button type="submit">Search</button>
+                  </form>
+              </section>
+          `;
+		}
 	}
 }
-
-customElements.define('ubanner', Banner);
 export default Banner;
+customElements.define('banner', Banner);
---
+++ src/data/dataProfiles.ts
@@ -0,0 +1,13 @@
+interface ProfilesShape {
+	Nickname: string;
+	Fullname: string;
+	ProfilePicture: string;
+}
+
+export const Profiles: ProfilesShape[] = [
+	{
+		Nickname: 'LAURIX',
+		Fullname: 'Laura Palacio',
+		ProfilePicture: 'https://i.pinimg.com/564x/0c/dc/42/0cdc42ae52de52c6ebc16f1f23b3dabd.jpg',
+	},
+];
