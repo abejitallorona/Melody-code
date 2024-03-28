5e2622ac79f520f878aac201bde914d544a300cc
majito <mariajorm2004@gmail.com>
2024-03-28 13:01:53 -0500

    style: formatting dataAlbums

--- src/data/dataAlbums.ts
+++ src/data/dataAlbums.ts
@@ -1,6 +1,6 @@
 const Albums = [
-	{ AlbumCover: 'https://i.scdn.co/image/ab67616d0000b27382877ed8854250100fb4a3f1', views: '68k', likes: '37k' },
-	{ AlbumCover: 'https://i.scdn.co/image/ab67616d0000b27382877ed8854250100fb4a3f1', views: '68k', likes: '37k' },
-	{ AlbumCover: 'https://i.scdn.co/image/ab67616d0000b27382877ed8854250100fb4a3f1', views: '68k', likes: '37k' },
-	{ AlbumCover: 'https://i.scdn.co/image/ab67616d0000b27382877ed8854250100fb4a3f1', views: '68k', likes: '37k' },
+	{ albumCover: 'https://i.scdn.co/image/ab67616d0000b27382877ed8854250100fb4a3f1', views: '68k', likes: '37k' },
+	{ albumCover: 'https://i.scdn.co/image/ab67616d0000b27382877ed8854250100fb4a3f1', views: '68k', likes: '37k' },
+	{ albumCover: 'https://i.scdn.co/image/ab67616d0000b27382877ed8854250100fb4a3f1', views: '68k', likes: '37k' },
+	{ albumCover: 'https://i.scdn.co/image/ab67616d0000b27382877ed8854250100fb4a3f1', views: '68k', likes: '37k' },
 ];
