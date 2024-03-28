66069d13d16f1ca7ff99b9344b4291dda398b02e
majito <mariajorm2004@gmail.com>
2024-03-28 14:05:42 -0500

    feat: dataAlbums

--- src/data/dataAlbums.ts
+++ src/data/dataAlbums.ts
@@ -1,4 +1,10 @@
-const Albums = [
+interface AlbumsShape {
+	albumCover: string;
+	views: string;
+	likes: string;
+}
+
+export const Albums: AlbumsShape[] = [
 	{ albumCover: 'https://i.scdn.co/image/ab67616d0000b27382877ed8854250100fb4a3f1', views: '68k', likes: '37k' },
 	{ albumCover: 'https://i.scdn.co/image/ab67616d0000b27382877ed8854250100fb4a3f1', views: '68k', likes: '37k' },
 	{ albumCover: 'https://i.scdn.co/image/ab67616d0000b27382877ed8854250100fb4a3f1', views: '68k', likes: '37k' },
